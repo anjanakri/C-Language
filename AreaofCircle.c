c6f4785689a4a1e95e6c298cf5da7126c5517ffe
Anjana Kumari <anjanakri.work@gmail.com>
2025-03-10 11:30:41 +0530

    Create AreaofCircle.c

---
+++ AreaofCircle.c
@@ -0,0 +1,11 @@
+//AREA OF CIRCLE
+#include<stdio.h>
+int main(){
+printf("Area of Circle \n");
+float B;
+printf("Enter Radius of circle");
+scanf("%f", &B);
+float AC=3.14*(B*B);
+printf("Print area of circle %3f",AC);
+return 0;
+}
